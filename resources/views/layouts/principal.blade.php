25abd662872965ec6b55aa1aa8e635866b7a08cb
edu2576 <bernal1021@gmail.com>
2015-08-16 18:17:24 -0500

    Calendario datetimepicker

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -17,6 +17,7 @@
  	{!!Html::style('sb-admin/bower_components/metisMenu/dist/metisMenu.min.css'); !!}
 	{!!Html::style('sb-admin/dist/css/sb-admin-2.css'); !!}
 	{!!Html::style('sb-admin/bower_components/font-awesome/css/font-awesome.min.css'); !!}
+	{!!Html::style('sb-admin/bower_components/datetimepicker/css/bootstrap-datetimepicker.min.css'); !!}
 	<style type="text/css" media="all">
 	    /* fix rtl for demo */
 	    .chosen-rtl .chosen-drop { left: -9000px; }
@@ -25,18 +26,14 @@
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 	{!!Html::script('assets/tutorial/js/ie10-viewport-bug-workaround.js'); !!}
 	{!!Html::script('assets/tutorial/js/ie-emulation-modes-warning.js'); !!}
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
 	{!!Html::script('assets/jquery-v2.1.4/jquery-2.1.4.min.js'); !!}
 	{!!Html::script('assets/jquery-jqGrid-v4.8.0/js/i18n/grid.locale-en.js'); !!}
 	{!!Html::script('assets/jquery-jqGrid-v4.8.0/js/jquery.jqGrid.src.js'); !!}
 	{!!Html::script('assets/bootstrap-v3.3.5/js/bootstrap.min.js'); !!}
 	{!!Html::script('assets/tutorial/js/helpers.js'); !!}
 	{!!Html::script('assets/tutorial/js/base.js'); !!}
+	{!!Html::script('sb-admin/bower_components/datetimepicker/js/moment.js'); !!}
+	{!!Html::script('sb-admin/bower_components/datetimepicker/js/bootstrap-datetimepicker.min.js'); !!}
 	{!!Html::script('choosen/chosen.jquery.js'); !!}
   	{!!Html::script('choosen/docsupport/prism.js'); !!}
 
