06112b7f1041f05ae21a3e43b5f0cb0e41386011
sistemasiblu <sistemas@ciiblu.com>
2016-10-13 16:53:19 -0500

    Correccion Resultado de Diagnostico en Grid

--- public/ajax/datosDiagnostico.php
+++ public/ajax/datosDiagnostico.php
@@ -29,6 +29,26 @@
             ->groupby('idDiagnostico')
             ->get();
 
+
+    $diagnostico = DB::select(
+        'SELECT D.idDiagnostico, D.codigoDiagnostico, D.nombreDiagnostico, D.fechaElaboracionDiagnostico, AVG(DR.resultadoDiagnosticoDetalle) as resultadoDiagnosticoDetalle
+        FROM diagnostico D
+        LEFT JOIN 
+        (
+            SELECT idDiagnostico, AVG(resultadoDiagnosticoDetalle) as resultadoDiagnosticoDetalle
+            FROM diagnostico D
+            LEFT JOIN diagnosticodetalle DD
+                ON D.idDiagnostico =  DD.Diagnostico_idDiagnostico
+            LEFT JOIN diagnosticopregunta DP
+                ON DD.DiagnosticoPregunta_idDiagnosticoPregunta = DP.idDiagnosticoPregunta
+            WHERE D.Compania_idCompania = '. \Session::get('idCompania').'  
+            GROUP BY D.idDiagnostico, DiagnosticoGrupo_idDiagnosticoGrupo
+        ) DR
+            ON D.idDiagnostico =  DR.idDiagnostico
+        WHERE D.Compania_idCompania = '. \Session::get('idCompania').'  
+        GROUP BY D.idDiagnostico');
+
+
     $row = array();
 
     foreach ($diagnostico as $key => $value) 
@@ -46,7 +66,7 @@
         $row[$key][] = $value->codigoDiagnostico;
         $row[$key][] = $value->nombreDiagnostico; 
         $row[$key][] = $value->fechaElaboracionDiagnostico;
-        $row[$key][] = $value->resultadoDiagnosticoDetalle;    
+        $row[$key][] = '<label style="text-align:right; width:100%;">'.number_format($value->resultadoDiagnosticoDetalle,1,'.',',').'%</label>';    
     }
 
     $output['aaData'] = $row;
