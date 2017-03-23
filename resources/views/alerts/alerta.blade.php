dfce6d04e7be6da5fecb2e2f9948901c1e3e2aa5
Juan Laverde <Juan Laverde>
2017-03-23 16:40:19 -0500

    Tercero Alerta, matrizRiesgo

---
+++ resources/views/alerts/alerta.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.vista')
+@section('titulo')<h3 id="titulo"><center>¡¡Alerta!!</center></h3>@stop
+@section('content')
+@include('alerts.request')
+
+
+
+
+                          
+<div class="alerta-container">
+      <form class="form-horizontal" action="" method="post">
+         <legend class="text">
+         <?php
+          echo'Esta intentando eliminar un'.' '.'<b>'.$tercero.'</b>'.' '.'que est&#225; relacionado en:<br>'.' '.'<b>'.$tablas.'</b>' 
+         ?>
+         
+         </legend>    
+          <!-- Se hace un boton para dar atras con laravel-->
+              <a href="{{ URL::previous() }}" class="btn btn-primary">Aceptar</a>     
+       </form>
+</div> 
+
+    
+   @stop
+   
+
+        
+
+                   
