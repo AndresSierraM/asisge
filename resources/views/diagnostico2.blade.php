ece9ae142db611ddbdc335537b537f4e7e06c134
Juan Laverde <mateomoralesgil@hotmail.com>
2017-07-06 10:29:50 -0500

    Diagnostico2

--- resources/views/diagnostico2.blade.php
+++ resources/views/diagnostico2.blade.php
@@ -234,7 +234,7 @@
                                   <tbody>
 
                                         <tr>
-                                          <td style=" background-color:#CACACA;" '.$datos[$i]['numeroDiagnosticoNivel4'].' <b>'.$datos[$i]['tituloDiagnosticoNivel4'].'</b></td>
+                                          <td style=" background-color:#CACACA;" <b>'.$datos[$i]['numeroDiagnosticoNivel4'].' '.$datos[$i]['tituloDiagnosticoNivel4'].'</b></td>
                                           <td style=" background-color:#CACACA;"><input type="text" id="puntuacionDiagnostico2Detalle'.$i.'" name="puntuacionDiagnostico2Detalle[]" value="'.$datos[$i]['valorDiagnosticoNivel4'].'" readonly="readonly">                                          
                                           <input type="hidden" id="idDiagnosticoNivel1_'.$i.'" name="idDiagnosticoNivel1[]" value="'.$datos[$i]["idDiagnosticoNivel1"].'" >
                                           <input type="hidden" id="idDiagnosticoNivel2_'.$i.'" name="idDiagnosticoNivel2[]" value="'.$datos[$i]["idDiagnosticoNivel2"].'" >
